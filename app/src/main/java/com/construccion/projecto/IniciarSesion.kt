2c40aa5c51aa449616b9bbd2c4bb7d6ef51682e6
usuario1 <usuario1@M2-305-27-11386.utadeo.loc>
2024-10-24 20:58:34 -0500

    4

--- app/src/main/java/com/construccion/projecto/IniciarSesion.kt
+++ app/src/main/java/com/construccion/projecto/IniciarSesion.kt
@@ -2,6 +2,8 @@ package com.construccion.projecto
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -24,5 +26,20 @@ class IniciarSesion : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
+    }
+    private fun loginUser() {
+        val emailOusuario = findViewById<EditText>(R.id.usuarioOcorreo).text.toString().trim()
+        val contraseña = findViewById<EditText>(R.id.contraseñaCampoInicio).text.toString().trim()
+
+        if (emailOusuario.isEmpty() || contraseña.isEmpty()) {
+            Toast.makeText(this, "Por favor llena todos los campos", Toast.LENGTH_SHORT).show()
+            return
+        } else {
+            Toast.makeText(this, "Por favor llena todos los campos", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+
     }
 }
